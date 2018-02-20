8e95362b8a133469575878376a5d0bd51c5e291c
RoddieFrederik <35548727+RoddieFrederik@users.noreply.github.com>
2018-02-19 21:38:41 -0600

    Update User.java

--- User.java
+++ User.java
@@ -5,6 +5,15 @@ public class User {
     private String username;
     private String password;
     private String bio;
+    
+    public User() {
+    }
+    
+    public User(String un, String pw, String b) {
+        this.username = un;
+        this.password = pw;
+        this.bio = b;
+    }
 
     public void setPassword(String pw) {
         this.password = pw;
